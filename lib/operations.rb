0bdbb1e8d7752f09cb554554b079dee217eb3033
Seth Near <seth.j.near@gmail.com>
2020-06-13 19:59:36 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,5 +1,5 @@
 def unsafe?(speed)
-  if (40 > speed > 60)
+  if (speed > 60) || (speed < 40)
     return true
   else
     return false
@@ -9,8 +9,5 @@ end
 
 
 def not_safe?(speed)
-	
+
 end
-	
-
-
